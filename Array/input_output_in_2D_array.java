f0a819fb68274e99314a9197521c09b2a8f3d93d
Ayush <87942664+iayushsahu@users.noreply.github.com>
2022-05-08 22:57:19 +0530

    Add files via upload

---
+++ Array/input_output_in_2D_array.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Main {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int rows = sc.nextInt();
+        int cols = sc.nextInt();
+        int marks[][] = new int[rows][cols];
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                marks[i][j] = sc.nextInt();
+            }
+        }
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                System.out.print(marks[i][j] +" ");
+            }
+            System.out.println();
+        }
+    }
+}
